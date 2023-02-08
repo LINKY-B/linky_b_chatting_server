dfbfc424901c6365cc441603293d8ce845dc4d39
DongwookKim0823 <dwkim0823@naver.com>
2023-02-09 00:38:38 +0900

    feat: send messages to subscriber

---
+++ src/main/java/com/linkyb/chat/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.linkyb.chat.controller;
+
+import com.linkyb.chat.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+@Slf4j
+public class ChatController {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat/message") // /pub/chat/message - 메세지 발행
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            message.setMessage(message.getSender() + "님이 입장했습니다.");
+        }
+        if (ChatMessage.MessageType.CLOSE.equals(message.getType())) {
+            message.setMessage("상대방이 채팅방을 나갔습니다.");
+        }
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message); // /sub/chat/room/1 - 구독
+
+    }
+}
