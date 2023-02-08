15b3c691896d3bff9567b89b454c8ba9796ee863
DongwookKim0823 <dwkim0823@naver.com>
2023-02-08 23:57:12 +0900

    feat: create ChatMessage dto class

---
+++ src/main/java/com/linkyb/chat/dto/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.linkyb.chat.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    public enum MessageType { //메세지 타입 : 입장, 채팅, 퇴장
+        ENTER, TALK, CLOSE
+    }
+    private MessageType type; // 메세지 타입
+    private String roomId; // 방 번호
+    private String sender; // 메세지 보낸 사람
+    private String message; // 메세지
+}
