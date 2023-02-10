cb2830ba3ed21014539f26a0ebbaef3acfd8ed48
DongwookKim0823 <dwkim0823@naver.com>
2023-02-11 01:57:05 +0900

    feat: add a send time field to the data to be saved

--- src/main/java/com/linkyb/chat/dto/ChatMessage.java
+++ src/main/java/com/linkyb/chat/dto/ChatMessage.java
@@ -14,4 +14,5 @@ public class ChatMessage {
     private String roomId; // 방 번호
     private String sender; // 메세지 보낸 사람
     private String message; // 메세지
+    private String sendingTime; // 메세지 전송 시간
 }
--- src/main/java/com/linkyb/chat/repository/ChatRepository.java
+++ src/main/java/com/linkyb/chat/repository/ChatRepository.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -18,7 +20,10 @@ public class ChatRepository {
     public void save(ChatMessage message) throws JsonProcessingException {
 
         ObjectMapper objectMapper = new ObjectMapper();
+        SimpleDateFormat format = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+        Date time = new Date();
 
+        message.setSendingTime(format.format(time));
         String messageJson = objectMapper.writeValueAsString(message);
         String roomId = message.getRoomId();
         mongoTemplate.insert(messageJson, roomId);
